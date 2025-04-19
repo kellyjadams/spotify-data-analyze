0fb38c1b7638346a9c249e14a50f1a232c254b55
Kelly Adams <81118926+kellyjadams@users.noreply.github.com>
2025-04-19 15:26:30 -0700

    FIX: Env variables in deploy to use SPOTIPY

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Load env vars from .env file
 source .env
 
 SERVICE_NAME="spotify-pipeline"
 REGION="us-central1"
 IMAGE="gcr.io/$GCP_PROJECT/$SERVICE_NAME"
 
+# Build Docker image
 gcloud builds submit --tag $IMAGE
 
+# Deploy to Cloud Run with all Spotipy + GCP + BigQuery environment variables
 gcloud run deploy $SERVICE_NAME \
   --image $IMAGE \
   --platform managed \
   --region $REGION \
   --allow-unauthenticated \
-  --set-env-vars SPOTIFY_CLIENT_ID=$SPOTIFY_CLIENT_ID,SPOTIFY_CLIENT_SECRET=$SPOTIFY_CLIENT_SECRET,SPOTIFY_REDIRECT_URI=$SPOTIFY_REDIRECT_URI,GCP_PROJECT=$GCP_PROJECT,BQ_DATASET=$BQ_DATASET,BQ_TABLE=$BQ_TABLE
+  --set-env-vars SPOTIPY_CLIENT_ID=$SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET=$SPOTIPY_CLIENT_SECRET,SPOTIPY_REDIRECT_URI=$SPOTIPY_REDIRECT_URI,SPOTIPY_REFRESH_TOKEN=$SPOTIPY_REFRESH_TOKEN,GCP_PROJECT=$GCP_PROJECT,BQ_DATASET=$BQ_DATASET,BQ_TABLE=$BQ_TABLE
