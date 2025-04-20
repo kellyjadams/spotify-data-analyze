24987e38abb31a6b878ddb4450372dc548d5096d
Kelly Adams <81118926+kellyjadams@users.noreply.github.com>
2025-04-19 20:04:37 -0700

    Update service account and don't allow unauthenticated

--- cloud/playback/deploy.sh
+++ cloud/playback/deploy.sh
@@ -1,19 +1,26 @@
-#!/bin/bash
-
-# Load environment variables from the root-level .env
+# Load environment variables
 source ../../.env
 
 SERVICE_NAME="spotify-playback-tracker"
 REGION="us-central1"
 IMAGE="gcr.io/$GCP_PROJECT/$SERVICE_NAME"
+SERVICE_ACCOUNT="spotify-to-bigquery@$GCP_PROJECT.iam.gserviceaccount.com"
 
 # Build Docker image
 gcloud builds submit --tag $IMAGE
 
-# Deploy to Cloud Run
+# Deploy to Cloud Run (secure, no key file, with service account)
 gcloud run deploy $SERVICE_NAME \
   --image $IMAGE \
   --platform managed \
   --region $REGION \
-  --allow-unauthenticated \
-  --set-env-vars SPOTIPY_CLIENT_ID=$SPOTIPY_CLIENT_ID,SPOTIPY_CLIENT_SECRET=$SPOTIPY_CLIENT_SECRET,SPOTIPY_REDIRECT_URI=$SPOTIPY_REDIRECT_URI,SPOTIPY_REFRESH_TOKEN=$SPOTIPY_REFRESH_TOKEN,GCP_PROJECT=$GCP_PROJECT,BQ_DATASET=$BQ_DATASET,BQ_TABLE=$BQ_TABLE
+  --service-account $SERVICE_ACCOUNT \
+  --no-allow-unauthenticated \
+  --set-env-vars \
+    SPOTIPY_CLIENT_ID=$SPOTIPY_CLIENT_ID,\
+    SPOTIPY_CLIENT_SECRET=$SPOTIPY_CLIENT_SECRET,\
+    SPOTIPY_REDIRECT_URI=$SPOTIPY_REDIRECT_URI,\
+    SPOTIPY_REFRESH_TOKEN=$SPOTIPY_REFRESH_TOKEN,\
+    GCP_PROJECT=$GCP_PROJECT,\
+    BQ_DATASET=$BQ_DATASET,\
+    BQ_TABLE=$BQ_TABLE
