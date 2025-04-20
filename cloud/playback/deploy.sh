f8601dff2cd8b18a80564509e08d106508096c7d
Kelly Adams <81118926+kellyjadams@users.noreply.github.com>
2025-04-19 20:27:24 -0700

    Update the environment variables

--- cloud/playback/deploy.sh
+++ cloud/playback/deploy.sh
@@ -1,5 +1,6 @@
 # Load environment variables
-source ../../.env
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+source "$SCRIPT_DIR/../../.env"
 
 SERVICE_NAME="spotify-playback-tracker"
 REGION="us-central1"
@@ -7,7 +8,7 @@ IMAGE="gcr.io/$GCP_PROJECT/$SERVICE_NAME"
 SERVICE_ACCOUNT="spotify-to-bigquery@$GCP_PROJECT.iam.gserviceaccount.com"
 
 # Build Docker image
-gcloud builds submit --tag $IMAGE
+gcloud builds submit cloud/playback --tag $IMAGE
 
 # Deploy to Cloud Run (secure, no key file, with service account)
 gcloud run deploy $SERVICE_NAME \
@@ -16,11 +17,10 @@ gcloud run deploy $SERVICE_NAME \
   --region $REGION \
   --service-account $SERVICE_ACCOUNT \
   --no-allow-unauthenticated \
-  --set-env-vars \
-    SPOTIPY_CLIENT_ID=$SPOTIPY_CLIENT_ID,\
-    SPOTIPY_CLIENT_SECRET=$SPOTIPY_CLIENT_SECRET,\
-    SPOTIPY_REDIRECT_URI=$SPOTIPY_REDIRECT_URI,\
-    SPOTIPY_REFRESH_TOKEN=$SPOTIPY_REFRESH_TOKEN,\
-    GCP_PROJECT=$GCP_PROJECT,\
-    BQ_DATASET=$BQ_DATASET,\
-    BQ_TABLE=$BQ_TABLE
+  --set-env-vars SPOTIPY_CLIENT_ID=$SPOTIPY_CLIENT_ID,\
+SPOTIPY_CLIENT_SECRET=$SPOTIPY_CLIENT_SECRET,\
+SPOTIPY_REDIRECT_URI=$SPOTIPY_REDIRECT_URI,\
+SPOTIPY_REFRESH_TOKEN=$SPOTIPY_REFRESH_TOKEN,\
+GCP_PROJECT=$GCP_PROJECT,\
+BQ_DATASET=$BQ_DATASET,\
+BQ_TABLE=$BQ_TABLE
