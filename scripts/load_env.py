5d2728adafc1587ed8426790df20841bb1e72035
Kelly Adams <81118926+kellyjadams@users.noreply.github.com>
2025-04-19 20:03:40 -0700

    Remove GOOGLE_APPLICATION_CREDENTIALS

--- scripts/load_env.py
+++ scripts/load_env.py
@@ -3,7 +3,6 @@ import os
 
 load_dotenv()
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 PROJECT_ID = os.getenv('GCP_PROJECT')
 DATASET_ID = os.getenv('BQ_DATASET')
 TABLE_ID = os.getenv('BQ_TABLE')
